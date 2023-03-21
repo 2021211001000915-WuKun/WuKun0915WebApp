eff379b3d4c033135936dfc3bbc58e9f97de51dd
zttttt <1253343770@qq.com>
2023-03-21 21:49:17 +0800

    week4

---
+++ src/main/java/com/WuKun/week4/homework/ConfigDemoServlet.java
@@ -0,0 +1,36 @@
+package com.WuKun.week4.homework;
+
+
+        import javax.servlet.*;
+        import javax.servlet.http.*;
+        import javax.servlet.annotation.*;
+        import java.io.IOException;
+        import java.io.PrintWriter;
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
+
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name="name",value="WuKun"),
+                @WebInitParam(name="studentId",value="2021211001000915"),
+        }
+)
+public class ConfigDemoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletConfig config=getServletConfig();
+        String name=config.getInitParameter("name");
+        String studentId=config.getInitParameter("studentId");
+        PrintWriter writer= response.getWriter();
+        writer.println("Name : "+name);
+        writer.println("StudentID : "+studentId);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
